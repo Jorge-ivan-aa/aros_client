34e397de3a24bf3c0c7625ab3770d66a185d01d0
Jorge-ivan-aa <jorgei.acostaa@uqvirtual.edu.co>
2025-10-07 12:36:59 -0500

    reset in dev branch for focus in only client development

--- server/scripts/clean_bd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo docker compose down
-sudo rm -r .data/
-sudo docker compose up &>/dev/null &
-
--- server/src/main/java/co/edu/uniquindio/comandera/domain/model/services/PasswordHasher.java
+++
@@ -1,6 +0,0 @@
-package co.edu.uniquindio.comandera.domain.model.services;
-
-public interface PasswordHasher {
-    String hash(String password);
-    boolean matches(String rawPassword, String hashedPassword);
-}
